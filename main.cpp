6607575802d9a8bafe4cc0a7c7349494326ba1c9
KostyaTelelyuhin <142903343+KostyaTelelyuhin@users.noreply.github.com>
2024-02-17 14:20:17 +0000

    +Bimfind

--- main.cpp
+++ main.cpp
@@ -13,22 +13,37 @@ int full_search(int a[], int N, int element){
     }
   }return -1;
 }
+int binary_search(int arr[], int low, int high, int target) {
+    if (low > high) {
+        return -1;
+    }
+
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == target) {
+        return mid;
+    } else if (arr[mid] > target) {
+        return binary_search(arr, low, mid - 1, target);
+    } else {
+        return binary_search(arr, mid + 1, high, target);
+    }
+}
 int main() {
-  int Numbers[10];
-  int time_do[10];
+  int Numbers[100];
+  int time_do[100];
   srand(time(0));
-  for (int i = 0;i<10;i++){
-    int N = rand() % 100 + 1; 
+  for (int i = 0;i<100;i++){
+    int N = rand() % 1000000 + 1; 
     Numbers[i] = N;
     int average = 0;
     for(int j = 0; j < 100; j++){
       int a[N];
       for(int k = 0; k < N; k++) {
-          a[k] = rand() % 1000 + 1; 
+          a[k] = rand() % 100 + 1; 
 
       }
 
-      int element =  rand() % 1000 + 1;
+      int element = 200;
       //cout <<  "elem = " << element;
 
       //Начало чeго-то
@@ -39,12 +54,12 @@ int main() {
       average = average + time_span.count();
       }
 
-    time_do[i] = average/5;
+    time_do[i] = average/100;
 
 
   }
 
-  for (int i = 0;i<10;i++){
+  for (int i = 0;i<100;i++){
     cout << Numbers[i] << " " << time_do[i] << endl;
   }
 
