9fedd7c72358d3940fe8aa41ab6caff972d92544
KostyaTelelyuhin <142903343+KostyaTelelyuhin@users.noreply.github.com>
2024-02-26 18:46:58 +0000

    Переработан рандом и сортировка

---
+++ .ccls-cache/@home@runner@Labsc-2/main.cpp
@@ -0,0 +1,424 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <stdlib.h>
+#include <random>
+#include <algorithm>
+#include <vector> 
+
+
+
+using namespace std;
+
+int MASSIVE[1000000];
+void mass(){
+  for(int i =0; i<1000000; i++){
+    MASSIVE[i]=i;
+  }
+}
+int func(){
+  return 0;
+}
+int time(){
+    auto begin = std::chrono::steady_clock ::now();
+    func();
+    auto end = chrono :: steady_clock ::now();
+    auto time_span = chrono :: duration_cast < chrono :: nanoseconds > (end - begin);
+    long long TIME_SPAN = time_span.count();
+    return TIME_SPAN;
+}
+
+int full_search(int a[], int N, int element){
+  for(int i = 0; i < N; i++){
+    if(a[i] == element){
+      return i;
+    } 
+  }
+
+  return -1;
+}
+/*
+uniform_int_distribution <unsigned> (10, 100);
+
+
+static default_random_engine *g_rng = nullptr;
+static uniform_int_distribution <unsigned> *g_dstr = nullptr;
+
+void init_my_rand(seed, min, max)
+{
+  g_rng = new std::default_random_engine(seed);
+  g_dstr = new uniform_int_distribution <unsigned> (min, max);
+}
+
+int get_my_rand()
+{
+  return g_dstr->operator()(*g_rng);
+}
+
+main()
+{
+  init_my_rand(..);
+
+  F1()
+
+  for ()
+    get_my_rand();
+}
+
+
+static std::default_random_engine g_eng(time(0));
+f()
+{
+  uniform_int_distribution <unsigned> di(10, 20);
+  int x = di(g_eng);
+  int x2 = di(g_eng);
+;}
+*/
+
+int random_number(int min, int max, unsigned seed = 100001){
+
+  default_random_engine rng ( seed ) ;
+  uniform_int_distribution <unsigned> dstr ( min , max ) ;
+  return MASSIVE[dstr(rng)];
+
+}
+
+
+int binary_search(int arr[], int low, int high, int target) {
+    if (low > high) {
+        return -1;
+    }
+
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == target) {
+        return mid;
+    } else if (arr[mid] > target) {
+        return binary_search(arr, low, mid - 1, target);
+    } else {
+        return binary_search(arr, mid + 1, high, target);
+    }
+}
+
+int search_sum(int a[], int N, int sum){
+  for (int i = 0; i < N - 1; i++){
+    //cout << "i = " <<i <<  endl;
+    //cout << "j  = ";
+    for(int j = i + 1; j < N; j++){
+      //cout  << j <<" ";
+
+      if(a[i] + a[j] == sum){
+        //cout << "i =  " << i << " j = " << j << endl;
+        return i;
+      }
+    }//cout << endl;
+  }//cout << -1 << endl;
+  return -1;
+
+}
+
+int search_bin_sum(int arr[], int low, int high, int summ){
+  if (low > high) {
+      //cout << "-1" << endl;
+      return -1;
+  }
+  if (summ > arr[high] + arr[low]){
+    return search_bin_sum(arr, low + 1, high, summ);
+  }else if (summ < arr[high] + arr[low]){
+    return search_bin_sum(arr, low , high - 1, summ);
+  }else if(summ == arr[high] + arr[low]){
+      //cout << "low = " << low << " high = " << high << endl;
+    return low;
+  }return 0;
+}
+
+
+
+int main_full_search(int All = 100,int reprize = 100,int MAX_COUNT = 1000000){
+
+  long long seed = 100001;
+  default_random_engine rng(seed);
+
+  uniform_int_distribution<unsigned> dstr(1, MAX_COUNT);
+  long long Numbers[All];
+  long long time_do[All];
+  for (int i = 0;i<All;i++){
+    long long N = MASSIVE[dstr(rng)];
+    uniform_int_distribution<unsigned> dstr1(0, N-1);
+    Numbers[i] = N;
+    int average = 0;
+    for(int j = 0; j < reprize; j++){
+      int a[N];
+     //Создание массива
+      for(int k = 0; k < N; k++) {
+          a[k] = MASSIVE[dstr(rng)];
+
+      }
+      // Элемент массива
+      int element = a[MASSIVE[dstr1(rng)]];  
+      //int element = -1;
+
+      auto begin = std::chrono::steady_clock ::now();
+      full_search(a, N, element);
+      auto end = chrono :: steady_clock ::now();
+      auto time_span = chrono :: duration_cast < chrono :: nanoseconds > (end - begin);
+      long long TIME_SPAN = time_span.count();
+      average = average + TIME_SPAN;
+      }
+
+    time_do[i] = average/reprize;
+
+
+  }
+
+  for (int i = 0;i<All;i++){
+    cout << Numbers[i] << " " << time_do[i] << endl;
+  }
+
+
+  return 0;
+}
+
+
+int main_bin_search(int All = 100,int reprize = 100,int MAX_COUNT = 1000000){
+  time();
+
+
+
+
+  long long seed = 100010;
+  default_random_engine rng(seed);
+  uniform_int_distribution<unsigned> dstr(1, MAX_COUNT - 1);
+
+  long long Numbers[All];
+  long long time_do[All];
+  for (int i = 0;i<All;i++){
+    long long N = MASSIVE[dstr(rng)];
+    uniform_int_distribution<unsigned> dstr1(0, N-1);
+    Numbers[i] = N;
+    int average = 0;
+
+    for(int j = 0; j < reprize; j++){
+      int a[N];
+     //Создание массива
+      //cout << "N = " << N << " Massive = ";
+      for(int k = 0; k < N; k++) {
+          a[k] = k;
+          //cout << a[k] << " ";
+      }
+      // Элемент массива
+      int element = a[dstr1(rng)];  
+      //int element = -1;
+      //cout << "element = " << element <<" i = " << binary_search(a, 0, N-1, element)<< endl;
+      auto begin = std::chrono::steady_clock ::now();
+      binary_search(a, 0, N-1, element);
+      auto end = chrono :: steady_clock ::now();
+      auto time_span = chrono :: duration_cast < chrono :: nanoseconds > (end - begin);
+      long long TIME_SPAN = time_span.count();
+      //cout << "TIME_SPAN = " << TIME_SPAN << " i = " << i << endl;
+
+
+      average = average + TIME_SPAN;
+
+      }
+      time_do[i] = average/reprize;
+
+
+  }
+
+  for (int i = 0;i<All;i++){
+    cout << Numbers[i] << " " << time_do[i] << endl;
+  }
+
+
+  return 0;
+}
+
+
+int main_search_sum(int All = 100,int reprize = 100,int MAX_COUNT = 1000000){
+  time();
+
+
+  long long seed = 100010;
+  default_random_engine rng(seed);
+  uniform_int_distribution<unsigned> dstr(1, MAX_COUNT - 1);
+
+  long long Numbers[All];
+  long long time_do[All];
+  for (int i = 0;i<All;i++){
+    long long N = MASSIVE[dstr(rng)];
+    uniform_int_distribution<unsigned> dstr1(0, N-1);
+    Numbers[i] = N;
+    int average = 0;
+
+    for(int j = 0; j < reprize; j++){
+      int a[N];
+     //Создание массива
+      //cout << "N = " << N << " Massive = ";
+      for(int k = 0; k < N; k++) {
+          a[k] =MASSIVE[dstr(rng)];
+          //cout << a[k] << " ";
+      }
+      // Элемент массива
+      int summ = MASSIVE[dstr(rng)];  
+      //int element = -1;
+      //cout << "summ = " << summ << endl;
+      auto begin = std::chrono::steady_clock ::now();
+      search_sum(a, N, summ);
+      auto end = chrono :: steady_clock ::now();
+      auto time_span = chrono :: duration_cast < chrono :: nanoseconds > (end - begin);
+      long long TIME_SPAN = time_span.count();
+      //cout << "TIME_SPAN = " << TIME_SPAN << " i = " << i << endl;
+
+
+      average = average + TIME_SPAN;
+
+      }
+      time_do[i] = average/reprize;
+
+
+  }
+
+  for (int i = 0;i<All;i++){
+    cout << Numbers[i] << " " << time_do[i] << endl;
+  }
+
+
+  return 0;
+}
+
+
+
+int main_bin_two_search(int All = 10,int reprize = 1,int MAX_COUNT = 10){
+  time();
+
+
+  long long seed = 100010;
+  default_random_engine rng(seed);
+  uniform_int_distribution<unsigned> dstr(1, MAX_COUNT - 1);
+
+  long long Numbers[All];
+  long long time_do[All];
+  for (int i = 0;i<All;i++){
+    long long N = MASSIVE[dstr(rng)];
+    uniform_int_distribution<unsigned> dstr1(0, N-1);
+    Numbers[i] = N;
+    int average = 0;
+
+    for(int j = 0; j < reprize; j++){
+      int a[N];
+     //Создание массива
+      //cout << "N = " << N << " Massive = ";
+      for(int k = 0; k < N; k++) {
+          a[k] = k;
+          //cout << a[k] << " ";
+      }
+      // Элемент массива
+      int summ = MASSIVE[dstr(rng)];  
+      //int element = -1;
+      //cout << "summ = " << summ << endl;
+      auto begin = std::chrono::steady_clock ::now();
+      search_bin_sum(a, 0, N-1, summ);
+      auto end = chrono :: steady_clock ::now();
+      auto time_span = chrono :: duration_cast < chrono :: nanoseconds > (end - begin);
+      long long TIME_SPAN = time_span.count();
+      //cout << "TIME_SPAN = " << TIME_SPAN << " i = " << i << endl;
+
+
+      average = average + TIME_SPAN;
+
+      }
+      time_do[i] = average/reprize;
+
+
+  }
+
+  for (int i = 0;i<All;i++){
+    cout << Numbers[i] << " " << time_do[i] << endl;
+  }
+
+
+  return 0;
+}
+
+
+
+
+
+int main() {
+  mass();
+  time();
+
+  // Зовем функцию, которая нужна(первые 2 пункта - первые две за поиск элемента, вторые 2 за поиск суммы)
+  //main_full_search();
+  //main_bin_search();
+
+  //main_search_sum();
+  //main_bin_two_search();
+
+
+
+
+
+
+
+
+  
+  /*
+  int All = 1000;
+  long long Numbers[All];
+  long long time_do[All];
+
+  long long MAX_COUNT = 1000000;
+  for (int i = 0;i<All;i++){
+    long long N = (RAND_MAX - rand()) * MAX_COUNT / RAND_MAX + 1; 
+    Numbers[i] = N;
+    int average = 0;
+    int reprize = 1000;
+    for(int j = 0; j < reprize; j++){
+      int a[N];
+     //Создание массива
+      for(int k = 0; k < N; k++) {
+          a[k] = (RAND_MAX - rand()) * MAX_COUNT / RAND_MAX + 1; 
+      }
+      x = 2*( (RAND_MAX - rand()) * MAX_COUNT / RAND_MAX + 1);
+
+
+      //cout << "x = " << x << endl;
+      //сортировка массивка(только для бинпоиска)
+      //qsort (a, N, sizeof(int), comp1);
+
+      // Элемент массива
+      //int element = a[(RAND_MAX - rand()) * N / RAND_MAX] ;     
+      //int element = -1;
+      //Начало чeго-то
+     // auto begin = std::chrono::steady_clock ::now();
+      //int TIME_SPAN  = full_search(a, N, element);
+
+      //binary_search(a, 0, N - 1, element);
+
+      //auto end = chrono :: steady_clock ::now();
+      //auto time_span = chrono :: duration_cast < chrono :: nanoseconds > (end - begin);
+      auto begin = std::chrono::steady_clock ::now();
+      search_sum(a, N, x);
+      auto end = chrono :: steady_clock ::now();
+      auto time_span = chrono :: duration_cast < chrono :: microseconds > (end - begin);
+      long long TIME_SPAN = time_span.count();
+      average = average + TIME_SPAN;
+      }
+
+    time_do[i] = average/reprize;
+
+
+  }
+
+  for (int i = 0;i<All;i++){
+    cout << Numbers[i] << " " << time_do[i] << endl;
+  }
+
+  */
+
+}
+
+
